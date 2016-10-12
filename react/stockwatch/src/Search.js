6f11b1aad1123a80d67a5a0f6c2a13c043c2a000
krskog <k.skog@online.no>
2016-10-12 13:44:10 +0200

    search nav

---
+++ react/stockwatch/src/Search.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+
+  render() {
+    return (
+      <div className="App">
+          <div className="App-header">
+            <h2>Søk</h2>
+          </div>
+        </div>
+    );
+  }
+}
+
+export default Search;
