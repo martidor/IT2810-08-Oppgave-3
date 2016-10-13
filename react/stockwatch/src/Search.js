d31a863682da07488eff461f3ebd386d5e980058
krskog <k.skog@online.no>
2016-10-13 10:33:02 +0200

    pasta search

--- react/stockwatch/src/Search.js
+++ react/stockwatch/src/Search.js
@@ -1,19 +1,47 @@
 import React, { Component } from 'react';
 import Searchbar from './components/Searchbar';
 import DisplaySearch from './components/DisplaySearch';
+import { Table, Grid, Row, Col } from 'react-bootstrap';
+import funds from './dummy-funds.json';
 import './components/search.css'
 class Search extends Component {
   
   render() {
     return (
-      <div className="App">
-          <div className="App-header">
-            <h2>Søk</h2>
-          </div>
-          <Searchbar />
-          <DisplaySearch />
+      <div>
+        <div className="searchhead">
+        <h2> Søk </h2>
+        <Searchbar />
         </div>
-
+          <Grid>
+            <Row className="show-grid">
+              <Col md={12}>
+                <Table hover responsive>
+                  <thead>
+                    <tr>
+                      <th>Navn</th>
+                      <th>Oppdatert</th>
+                      <th>Siste dag</th>
+                      <th>Avkastning</th>
+                      <th>Ann avk. %</th>
+                      <th>Total Verdi</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {
+                      funds.funds.map(function(fund) {
+                        return (
+                          <DisplaySearch fund={fund} />
+                        )
+                      })
+                    }
+                  </tbody>
+                </Table>
+              </Col>
+            </Row>
+          </Grid>
+        
+      </div>
     );
   }
 }
--- react/stockwatch/src/components/DisplaySearch.js
+++ react/stockwatch/src/components/DisplaySearch.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
-
+import './search.css';
+import './FundRow.css';
 class DisplaySearch extends Component {
   
+  getClassName(property) {
+    if (property < 0) 
+      return "red";
+    else if (property > 0)
+      return "green";
+    else return "";
+  }
+
   render() {
     return (
-      <div className="displaysearch">
-      	<p> searchresult#1</p>
-      	<p> searchresult#2</p>
-      </div>
-
+      <tr>
+        <td>{this.props.fund.name}</td>
+        <td>{this.props.fund.dateUpdated}</td>
+        <td className={this.getClassName(this.props.fund.percentChanged)}>{this.props.fund.percentChanged}%</td>
+        <td>{this.props.fund.return} kr</td>
+        <td className={this.getClassName(this.props.fund.annualPercentReturn)}>{this.props.fund.annualPercentReturn}%</td>
+        <td>{this.props.fund.totalValue} kr</td>
+      </tr>
     );
   }
 }
