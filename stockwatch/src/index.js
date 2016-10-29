c0062bf7d49546870adb2abc97dce313111eb2fd
Eirik Fosse <eirik@bustbyte.no>
2016-10-29 23:39:39 +0200

    Added libraries to control currency, date formatting, time formatting, number formatting etc.

--- stockwatch/src/index.js
+++ stockwatch/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { IntlProvider, addLocaleData } from 'react-intl';
+import noLocaleData from 'react-intl/locale-data/no';
 import App from './App';
 import Overview from './overview/Overview';
 import Profile from './profile/Profile';
@@ -10,18 +12,22 @@ import NotFound from './notfound/NotFound'
 import Search from './search/Search';
 import './index.css';
 
+addLocaleData(noLocaleData);
+
 // This methods initializes the routing on the page.
 render(
 	(
-	  <Router history={browserHistory}>
-	    <Route path="/" component={App}>
-	    	<IndexRoute component={Home} name="Børsoversikten"/>
-	    	<Route path="oversikt" component={Overview} name="Oversikt"/>
-			<Route path="profil" component={Profile} name="Profil"/>	
-	    	<Route path="sok" component={Search} name="Søk"/> 
-	    	<Route path="logg-inn" component={Login} name="Logg inn"/> 
-	    	<Route path="*" component={NotFound} name="Ikke funnet"/>
-	    </Route>
-	  </Router>
+		<IntlProvider locale="no">
+		  <Router history={browserHistory}>
+		    <Route path="/" component={App}>
+		    	<IndexRoute component={Home} name="Børsoversikten"/>
+		    	<Route path="oversikt" component={Overview} name="Oversikt"/>
+				<Route path="profil" component={Profile} name="Profil"/>	
+		    	<Route path="sok" component={Search} name="Søk"/> 
+		    	<Route path="logg-inn" component={Login} name="Logg inn"/> 
+		    	<Route path="*" component={NotFound} name="Ikke funnet"/>
+		    </Route>
+		  </Router>
+	  	</IntlProvider>
 	), document.getElementById('root')
 );
