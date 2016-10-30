c45db0cd80bbcd3225977d52191745683faa1560
Eirik Fosse <eirik@bustbyte.no>
2016-10-30 20:57:14 +0100

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,6 @@ sudo kill -9 $(pidof nohup)	# kill nohup process
 sudo kill -9 $(pidof node) 	# kill node process to stop the server
 sudo git pull origin master 	# pull down the latest changes using git
 sudo npm --prefix ./stockwatch install ./stockwatch #react
+sudo npm --prefix ./api install ./api #api
+sudo nohup npm --prefix ./api start & # run the api a background process
 sudo PORT=80 nohup npm --prefix ./stockwatch start & # run the server as a background process on port 80
