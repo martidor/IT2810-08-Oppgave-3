31fbbbde46b7342a81c76517d4ebe550b99424de
Eirik Fosse <eirik@bustbyte.no>
2016-11-02 13:49:46 +0100

    Set up routes for passport and Facebook auth.

---
+++ api/routes/auth.js
@@ -0,0 +1,39 @@
+var express 	= require('express');
+
+// ROUTES FOR OUR AUTH
+// =============================================================================
+
+// create our router
+var authRotuer = express.Router();
+
+
+// middleware to use for all requests
+authRotuer.use(function(req, res, next) {
+	// set headers
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header("Content-Type", "application/json");
+
+	console.log('Perform authentication here.');
+	next();
+});
+
+// test route to make sure everything is working (accessed at GET /api)
+authRotuer.get('/', function(req, res) {
+	res.json({message: 'Auth API is up and running.'});
+});
+
+// Redirect the user to Facebook for authentication.  When complete,
+// Facebook will redirect the user back to the application at
+//     /auth/facebook/callback
+authRotuer.get('/facebook', passport.authenticate('facebook'));
+
+// Facebook will redirect the user to this URL after approval.  Finish the
+// authentication process by attempting to obtain an access token.  If
+// access was granted, the user will be logged in.  Otherwise,
+// authentication has failed.
+authRotuer.get('/facebook/callback',
+  passport.authenticate('facebook', { successRedirect: '/',
+                                      failureRedirect: '/login' }));
+
+module.exports = authRotuer;
--- api/server.js
+++ api/server.js
@@ -6,6 +6,22 @@ var express		= require('express');
 var app        	= express();
 var bodyParser 	= require('body-parser');
 var routes 		= require('./routes/index');
+var authRoutes	= require('./routes/auth');
+var passport 	= require('passport')
+var FacebookStrategy = require('passport-facebook').Strategy;
+
+passport.use(new FacebookStrategy({
+    clientID: FACEBOOK_APP_ID,
+    clientSecret: FACEBOOK_APP_SECRET,
+    callbackURL: "http://www.example.com/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    User.findOrCreate(..., function(err, user) {
+      if (err) { return done(err); }
+      done(null, user);
+    });
+  }
+));
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -14,8 +30,11 @@ app.use(bodyParser.json());
 
 var port = process.env.PORT || 8008;        // set our port
 
-// all of our routes will be prefixed with /api
+// API routes will be prefixed with /api
 app.use('/api', routes);
+
+// Auth routes
+app.use('/auth', authRoutes);
 
 // START THE SERVER
 // =============================================================================
