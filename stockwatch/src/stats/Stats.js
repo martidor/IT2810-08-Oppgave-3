41c56a96f3877cc66bc22e591d3d51489e1683dd
Eirik Fosse <eirik@bustbyte.no>
2016-11-03 01:17:01 +0100

    Fetch and display stats.

--- stockwatch/src/stats/Stats.js
+++ stockwatch/src/stats/Stats.js
@@ -1,16 +1,55 @@
 import React, { Component } from 'react';
 import FontAwesome from 'react-fontawesome';
-//import Chart from '../components/highcharts/Chart';
+import Chart from '../components/highcharts/Chart';
+import './Stats.css';
 
 class Stats extends Component {
 
 	// eslint-disable-next-line
     constructor(props){
         super(props);
+
+	    // Initialize the state with no equities and modal not showing.
+	    this.state = {
+	      statsLoaded: false,
+	      stats: {}
+	    };
+
+	    this.loadStats = this.loadStats.bind(this)
+	    this.statsLoaded = this.statsLoaded.bind(this)
+
+	    this.loadStats(this.statsLoaded);
     }
 
+    loadStats(callback){
+	    // User id hard coded for now.
+	    return fetch(window.apiUrl + '/user/1/stats')
+	      .then((response) => response.json())
+	      .then((json) => {
+	        callback(json);
+	      })
+	      .catch((error) => {
+	        console.error(error);
+	    });
+	  }
+
+	  statsLoaded(json){
+	    this.setState({
+	      'statsLoaded': true,
+	      'stats': json
+	    });
+	  }
+
 	render() {
-        return (<div className="loading-home"><FontAwesome spin name="circle-o-notch" /> </div>)
+        return (
+        	this.state.statsLoaded ?
+        		<div className="charts">
+	            	<Chart container="return-chart" chartKey="return-on-investment" data={this.state.stats.return}/>
+	            	<Chart container="value-chart" chartKey="invested-and-value" data={this.state.stats}/>
+            	</div>
+            : 
+            <div className="loading-home"><FontAwesome spin name="circle-o-notch" /> </div>
+    	)
     }
  }
 
