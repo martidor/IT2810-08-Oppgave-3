6c779616b622fbe8b1dc9567fd46b4670b08e04b
Eirik Fosse <eirik@bustbyte.no>
2016-11-11 12:27:02 +0100

    Ignore main database.

--- api/database/database.js
+++ api/database/database.js
@@ -3,7 +3,7 @@ var sqlite3 = require('sqlite3').verbose();
 class Database{
 
 	static getDatabase(){
-		return new sqlite3.Database('./database/stockwatch.db');
+		return new sqlite3.Database('./database/database.db');
 	}
 
 	static getPublicUsers(callback){
