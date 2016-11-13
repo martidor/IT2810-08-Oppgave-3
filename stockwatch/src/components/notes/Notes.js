25603e964b9dba1a30fe2616df676b0133cca8b6
corneliusgdahling <corneliusdahling@gmail.com>
2016-11-13 19:41:27 +0100

    Added comments

--- stockwatch/src/components/notes/Notes.js
+++ stockwatch/src/components/notes/Notes.js
@@ -8,6 +8,13 @@ import auth from '../../auth/auth';
 
 export default class Notes extends Component{
 
+/*
+  This component is a text area where users can enter notes that are stored automatically.
+  It is rendered on all pages when the user is logged in.
+*/
+
+
+//Hides and shows the note field based on whether the user is logged in.
   toggleNoteField = () => {
     this.setState({
       showNoteField: !this.state.showNoteField,
@@ -15,15 +22,18 @@ export default class Notes extends Component{
     });
   }
 
+//Removes the listener to update auth when the component is removed from the DOM.
   componentWillUnmount(){
     auth.removeListener(this.updateAuth);
   }
 
+//Adds listener to update auth right before mounting occurs.
   componentWillMount(){
     auth.addListener(this.updateAuth);
     this.updateAuth();
   }
 
+//Checks if user is logged in and updates the isLoggedIn state based on that result.
   updateAuth = () => {
     this.setState({isLoggedIn: auth.isLoggedIn()});
   }
