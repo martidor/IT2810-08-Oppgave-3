70a2821400fe58f6720f20524d17aad5e6f18590
corneliusgdahling <corneliusdahling@gmail.com>
2016-11-13 20:16:55 +0100

    Fixed formatting FormattedDateTime

--- stockwatch/src/components/format/FormattedDateTime.js
+++ stockwatch/src/components/format/FormattedDateTime.js
@@ -13,7 +13,7 @@ export default class FormattedDateTime extends Component{
     if (this.props.prefix)
       possiblePrefix = (<span>kl </span>);
 
-    // If it is a share and the date is today, show minutes
+      // If it is a share and the date is today, show minutes
       if (this.props.type === "SHARES" && moment(this.props.timestamp).isSame(Date.now(), 'day'))
       return (
         <div>
@@ -23,8 +23,8 @@ export default class FormattedDateTime extends Component{
           />
         </div>
       )
-    // Otherwise, show a date
-    else
+      // Otherwise, show a date
+      else
       return (
         <FormattedDate value={new Date(this.props.timestamp)} />
       )
